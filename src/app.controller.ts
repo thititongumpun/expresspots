0e86c774dc9bf17eb9ba7f126229f8f2f125c712
thititongumpun <thiti180536@gmail.com>
2023-10-07 22:19:46 +0700

    change tsconfig

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,20 +2,23 @@ import { BaseController } from "@expressots/core";
 import { Response } from "express";
 import { AppUseCase } from "./app.usecase";
 import { Get, controller, response } from "@expressots/adapter-express";
+import ENV from "./env";
 
 @controller("/")
 export class AppController extends BaseController {
-    constructor(private appUseCase: AppUseCase) {
-        super();
-    }
+  constructor(private appUseCase: AppUseCase) {
+    super();
+  }
 
-    @Get("/")
-    execute(@response() res: Response) {
-        return res.send(this.appUseCase.execute());
-    }
+  @Get("/")
+  execute(@response() res: Response) {
+    console.log(ENV.Application.PORT);
+    // console.log(process.env.PORT);
+    return res.send(this.appUseCase.execute());
+  }
 
-    @Get("test")
-    getTest(@response() res: Response) {
-      return res.json(this.appUseCase.getTest());
-    }
+  @Get("test")
+  getTest(@response() res: Response) {
+    return res.json(this.appUseCase.getTest());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import "reflect-metadata";
 import { AppFactory } from "@expressots/core";
 import { container } from "./app.container";
 import { ServerEnvironment } from "@expressots/adapter-express";
+import { App } from "./providers/application/application.provider";
+import ENV from "./env";
 
 async function bootstrap() {
-    const app = await AppFactory.create(container, []);
-    await app.listen(3000, ServerEnvironment.Development, {
-      appName: "devtoscraping",
-      appVersion: "v1.0.0",
+    const app = await AppFactory.create(container, App);
+    await app.listen(ENV.Application.PORT, ServerEnvironment.Development, {
+      appName: ENV.Application.APP_NAME,
+      appVersion: ENV.Application.APP_VERSION,
     });
 }
 
